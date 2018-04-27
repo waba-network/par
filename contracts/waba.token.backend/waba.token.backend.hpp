45f9b828f7082016212ff1a8d89367bf30f03779
Ariel Scarpinelli <ascarpinelli@spredfast.com>
2018-04-27 16:43:47 -0300

    possible implementation of settings per contract type

--- contracts/waba.token.backend/waba.token.backend.hpp
+++ contracts/waba.token.backend/waba.token.backend.hpp
@@ -2,90 +2,89 @@
 
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
-#include <eosiolib/eosio.hpp>
 
 #include <string>
 
+#include "token_contract.hpp"
+#include "mutual_credit_system_contract.hpp"
+
 namespace waba {
 
-   using std::string;
-   using eosio::asset;
-   using eosio::symbol_name;
+    using std::string;
+    using std::vector;
+    using eosio::asset;
+    using eosio::symbol_name;
+    using eosio::symbol_type;
 
-   class token : public eosio::contract {
-      public:
-         token( account_name self ):contract(self){}
+    typedef struct token_settings {
+        asset supply;
+        account_name issuer;
+        contract_type contract_type;
 
-         void create( account_name issuer,
-                      asset        maximum_supply,
-                      uint8_t      issuer_can_freeze,
-                      uint8_t      issuer_can_recall,
-                      uint8_t      issuer_can_whitelist );
+        uint64_t primary_key() const { return supply.symbol.name(); }
+    } token_settings;
+
+    class token : public eosio::contract {
+    public:
+        token(account_name self) : contract(self) {}
+
+        void create(account_name issuer,
+                    symbol_type symbol,
+                    contract_type type,
+                    vector<contract_setting> contract_settings);
+
+        void issue(account_name to, asset quantity, string memo);
+
+        void transfer(account_name from,
+                      account_name to,
+                      asset quantity,
+                      string memo);
+
+    private:
+
+        inline asset get_supply(symbol_name sym) const;
+
+        inline asset get_balance(account_name owner, symbol_name sym) const;
+
+    private:
+        struct account {
+            asset balance;
+            bool frozen = false;
+            bool whitelist = true;
+
+            uint64_t primary_key() const { return balance.symbol.name(); }
+        };
 
 
-         void issue( account_name to, asset quantity, string memo );
+        typedef eosio::multi_index<N(accounts), account> accounts;
+        typedef eosio::multi_index<N(token), token_settings> token_settings_table;
 
-         void transfer( account_name from,
-                        account_name to,
-                        asset        quantity,
-                        string       memo );
+        void sub_balance(account_name owner, asset value, const token_settings &st);
 
-      private:
+        void add_balance(account_name owner, asset value, const token_settings &st,
+                         account_name ram_payer);
 
-         inline asset get_supply( symbol_name sym )const;
-         
-         inline asset get_balance( account_name owner, symbol_name sym )const;
+    public:
+        struct transfer_args {
+            account_name from;
+            account_name to;
+            asset quantity;
+            string memo;
+        };
 
-      private:
-         struct account {
-            asset    balance;
-            bool     frozen    = false;
-            bool     whitelist = true;
+        token_contract get_contract(contract_type type);
+    };
 
-            uint64_t primary_key()const { return balance.symbol.name(); }
-         };
+    asset token::get_supply(symbol_name sym) const {
+        token_settings_table token_settings_table(_self, sym);
+        const auto &st = token_settings_table.get(sym);
+        return st.supply;
+    }
 
-         struct currency_stats {
-            asset          supply;
-            asset          max_supply;
-            account_name   issuer;
-            bool           can_freeze         = true;
-            bool           can_recall         = true;
-            bool           can_whitelist      = true;
-            bool           is_frozen          = false;
-            bool           enforce_whitelist  = false;
-
-            uint64_t primary_key()const { return supply.symbol.name(); }
-         };
-
-         typedef eosio::multi_index<N(accounts), account> accounts;
-         typedef eosio::multi_index<N(stat), currency_stats> stats;
-
-         void sub_balance( account_name owner, asset value, const currency_stats& st );
-         void add_balance( account_name owner, asset value, const currency_stats& st,
-                           account_name ram_payer );
-
-      public:
-         struct transfer_args {
-            account_name  from;
-            account_name  to;
-            asset         quantity;
-            string        memo;
-         };
-   };
-
-   asset token::get_supply( symbol_name sym )const
-   {
-      stats statstable( _self, sym );
-      const auto& st = statstable.get( sym );
-      return st.supply;
-   }
-
-   asset token::get_balance( account_name owner, symbol_name sym )const
-   {
-      accounts accountstable( _self, owner );
-      const auto& ac = accountstable.get( sym );
-      return ac.balance;
-   }
+    asset token::get_balance(account_name owner, symbol_name sym) const {
+        accounts accountstable(_self, owner);
+        const auto &ac = accountstable.get(sym);
+        return ac.balance;
+    }
 
 } /// namespace eosio
